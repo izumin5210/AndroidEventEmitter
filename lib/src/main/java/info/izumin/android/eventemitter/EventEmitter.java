c213da22bbe20dd952e63a3372d99a3084bc4e8a
izumin5210 <masayuki@izumin.info>
2015-10-09 20:29:17 +0900

    Implement EventEmitter class

--- lib/src/main/java/info/izumin/android/eventemitter/EventEmitter.java
+++ lib/src/main/java/info/izumin/android/eventemitter/EventEmitter.java
@@ -1,4 +1,28 @@
 package info.izumin.android.eventemitter;
 
-public class EventEmitter {
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventEmitter<L> {
+    private List<L> mListeners;
+
+    public EventEmitter() {
+        mListeners = new ArrayList<>();
+    }
+
+    public void addListener(L listener) {
+        mListeners.add(listener);
+    }
+
+    public void on(L listener) {
+        addListener(listener);
+    }
+
+    public void removeListener(L listener) {
+        mListeners.remove(listener);
+    }
+
+    public void removeAllListeners() {
+        mListeners.clear();
+    }
 }
