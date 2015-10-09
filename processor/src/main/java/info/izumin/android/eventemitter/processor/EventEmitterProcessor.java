8216abc2abf1e4b5b818a920f725747cd32e12b3
izumin5210 <masayuki@izumin.info>
2015-10-09 23:42:41 +0900

    Implement EventEmitterProcessor

--- processor/src/main/java/info/izumin/android/eventemitter/processor/EventEmitterProcessor.java
+++ processor/src/main/java/info/izumin/android/eventemitter/processor/EventEmitterProcessor.java
@@ -1,4 +1,58 @@
 package info.izumin.android.eventemitter.processor;
 
-public class EventEmitterProcessor {
+import com.google.auto.service.AutoService;
+
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.tools.Diagnostic;
+
+import info.izumin.android.eventemitter.annotation.Listener;
+
+@AutoService(Processor.class)
+public class EventEmitterProcessor extends AbstractProcessor {
+
+    private Filer filer;
+    private Elements elementUtils;
+    private Messager messager;
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return new LinkedHashSet<String>() {{
+            add(Listener.class.getName());
+        }};
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        filer = processingEnv.getFiler();
+        elementUtils = processingEnv.getElementUtils();
+        messager = processingEnv.getMessager();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Set<? extends Element> elements =  roundEnv.getElementsAnnotatedWith(Listener.class);
+        for (Element element : elements) {
+            EventEmitterModel model = new EventEmitterModel(element, elementUtils);
+            EventEmitterWriter writer = new EventEmitterWriter(model);
+            try {
+                writer.write(filer);
+            } catch (IOException e) {
+                messager.printMessage(Diagnostic.Kind.ERROR, e.getMessage());
+            }
+        }
+        return true;
+    }
 }
